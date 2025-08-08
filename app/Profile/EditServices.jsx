f14be1ebd81131e42ab863822d85683000cb70b8
snowbi23 <ejuknasao@gmail.com>
2025-08-08 18:50:36 +0800

    Create EditServices.jsx

---
+++ app/Profile/EditServices.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EditServices = () => {
+  return (
+    <View>
+      <Text>EditServices</Text>
+    </View>
+  )
+}
+
+export default EditServices
+
+const styles = StyleSheet.create({})
