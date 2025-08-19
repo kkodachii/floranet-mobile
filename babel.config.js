366c96b401f7068263deca592b1cef7ed13d2391
zekie1604 <keanlucas54926@gmail.com>
2025-08-19 09:47:31 +0800

    fix: remove problematic babel plugin and clean up config to prevent app crashes

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@babel/plugin-syntax-flow',
       'react-native-reanimated/plugin'
     ]
   };
